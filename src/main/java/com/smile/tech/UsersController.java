01398298720f262ba246b3b6b0b6f57d219ed88f
padmanaban1996 <padmanaban.rc@kthree.in>
2021-03-26 16:20:42 +0530

    db changed

--- src/main/java/com/smile/tech/UsersController.java
+++ src/main/java/com/smile/tech/UsersController.java
@@ -6,6 +6,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.smile.tech.payload.request.UpdateUserRequest;
 import com.smile.tech.payload.request.UserCreateRequest;
 import com.smile.tech.service.UsersService;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/mirror")
 public class UsersController {
